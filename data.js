c974d493ff3d0698e0fd47ec1d806c17b683af59
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-21 00:04:12 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,113 +1,122 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-sts-philip--james-360-1",
+      "id": "0-sts-philip--james-catholic-church-360-view-1",
       "name": "360 View 1",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
-      "initialViewParameters": { "yaw": -0.002, "pitch": -0.06, "fov": 1.4 },
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.002, "pitch": -0.059, "fov": 1.4
+      },
       "linkHotspots": [
-        { "yaw": -0.01, "pitch": 0.407, "rotation": 0, "target": "2-sts-philip--james-360-3" },
-        { "yaw": -0.009, "pitch": 1.343, "rotation": 0, "target": "1-sts-philip--james-360-2" }
+        { "yaw": -0.010, "pitch": 0.407, "rotation": 0, "target": "2-sts-philip--james-catholic-church-360-view-3" },
+        { "yaw": -0.009, "pitch": 1.343, "rotation": 0, "target": "1-sts-philip--james-catholic-church-360-view-2" }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-sts-philip--james-360-2",
+      "id": "1-sts-philip--james-catholic-church-360-view-2",
       "name": "360 View 2",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
+      "faceSize": 1500,
       "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        { "yaw": 0.00006, "pitch": 0.019, "rotation": 0, "target": "2-sts-philip--james-360-3" },
-        { "yaw": -0.031, "pitch": -1.241, "rotation": 0, "target": "0-sts-philip--james-360-1" }
+        { "yaw": 0.00006, "pitch": 0.019, "rotation": 0, "target": "2-sts-philip--james-catholic-church-360-view-3" },
+        { "yaw": -0.031, "pitch": -1.241, "rotation": 0, "target": "0-sts-philip--james-catholic-church-360-view-1" }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-sts-philip--james-360-3",
+      "id": "2-sts-philip--james-catholic-church-360-view-3",
       "name": "360 View 3",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
+      "faceSize": 1500,
       "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        { "yaw": -3.129, "pitch": 0.225, "rotation": 0, "target": "1-sts-philip--james-360-2" },
-        { "yaw": -3.136, "pitch": -0.768, "rotation": 0, "target": "0-sts-philip--james-360-1" },
-        { "yaw": -0.014, "pitch": 0.231, "rotation": 0, "target": "3-sts-philip--james-360-4" }
+        { "yaw": -3.129, "pitch": 0.225, "rotation": 0, "target": "1-sts-philip--james-catholic-church-360-view-2" },
+        { "yaw": -3.136, "pitch": -0.768, "rotation": 0, "target": "0-sts-philip--james-catholic-church-360-view-1" },
+        { "yaw": -0.014, "pitch": 0.230, "rotation": 0, "target": "3-sts-philip--james-catholic-church-360-view-4" }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-sts-philip--james-360-4",
+      "id": "3-sts-philip--james-catholic-church-360-view-4",
       "name": "360 View 4",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
+      "faceSize": 1500,
       "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        { "yaw": -0.013, "pitch": 0.193, "rotation": 0, "target": "4-sts-philip--james-360-5" },
-        { "yaw": 3.133, "pitch": 0.211, "rotation": 0, "target": "2-sts-philip--james-360-3" }
+        { "yaw": -0.013, "pitch": 0.193, "rotation": 0, "target": "4-sts-philip--james-catholic-church-360-view-5" },
+        { "yaw": 3.133, "pitch": 0.211, "rotation": 0, "target": "2-sts-philip--james-catholic-church-360-view-3" }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-sts-philip--james-360-5",
+      "id": "4-sts-philip--james-catholic-church-360-view-5",
       "name": "360 View 5",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
+      "faceSize": 1500,
       "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        { "yaw": -3.127, "pitch": 0.253, "rotation": 0, "target": "3-sts-philip--james-360-4" },
-        { "yaw": 0.007, "pitch": 0.18, "rotation": 0, "target": "5-sts-philip--james-360-6" }
+        { "yaw": -3.127, "pitch": 0.253, "rotation": 0, "target": "3-sts-philip--james-catholic-church-360-view-4" },
+        { "yaw": 0.007, "pitch": 0.179, "rotation": 0, "target": "5-sts-philip--james-catholic-church-360-view-6" }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-sts-philip--james-360-6",
+      "id": "5-sts-philip--james-catholic-church-360-view-6",
       "name": "360 View 6",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
+      "faceSize": 1500,
       "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        { "yaw": 0.002, "pitch": 0.177, "rotation": 0, "target": "6-sts-philip--james-360-7" },
-        { "yaw": 3.119, "pitch": 0.358, "rotation": 0, "target": "4-sts-philip--james-360-5" }
+        { "yaw": 0.002, "pitch": 0.177, "rotation": 0, "target": "6-sts-philip--james-catholic-church-360-view-7" },
+        { "yaw": 3.119, "pitch": 0.358, "rotation": 0, "target": "4-sts-philip--james-catholic-church-360-view-5" }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-sts-philip--james-360-7",
+      "id": "6-sts-philip--james-catholic-church-360-view-7",
       "name": "360 View 7",
       "levels": [
         { "tileSize": 256, "size": 256, "fallbackOnly": true },
-        { "tileSize": 512, "size": 512 }
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 1024,
+      "faceSize": 1500,
       "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        { "yaw": -0.001, "pitch": 0.595, "rotation": 0, "target": "5-sts-philip--james-360-6" },
-        { "yaw": -0.0001, "pitch": 0.228, "rotation": 0, "target": "4-sts-philip--james-360-5" }
+        { "yaw": -0.001, "pitch": 0.595, "rotation": 0, "target": "5-sts-philip--james-catholic-church-360-view-6" },
+        { "yaw": -0.0001, "pitch": 0.228, "rotation": 0, "target": "4-sts-philip--james-catholic-church-360-view-5" }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Sts. Philip & James Church Meyersdale PA",
+  "name": "Sts. Philip & James Catholic Church Meyersdale PA",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
