6fba5bcd5551a36f3143a32a1d1559f96dfb7b4b
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-20 23:39:30 -0400

    Update data.js

--- data.js
+++ data.js
@@ -2,349 +2,116 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-sts-philip--james-catholic-church-360-view-1",
-      "name": "Sts. Philip & James Catholic Church 360 View 1",
+      "name": "360 View 1",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
+      "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": -0.002079477691696141,
-        "pitch": -0.05942583870883311,
-        "fov": 1.4134061960355204
+        "yaw": -0.002, "pitch": -0.059, "fov": 1.4
       },
       "linkHotspots": [
-        {
-          "yaw": -0.010675307292203229,
-          "pitch": 0.40757912304284005,
-          "rotation": 0,
-          "target": "2-sts-philip--james-catholic-church-360-view-3"
-        },
-        {
-          "yaw": -0.009827099064857236,
-          "pitch": 1.342638935748985,
-          "rotation": 0,
-          "target": "1-sts-philip--james-catholic-church-360-view-2"
-        }
+        { "yaw": -0.010, "pitch": 0.407, "rotation": 0, "target": "2-sts-philip--james-catholic-church-360-view-3" },
+        { "yaw": -0.009, "pitch": 1.343, "rotation": 0, "target": "1-sts-philip--james-catholic-church-360-view-2" }
       ],
       "infoHotspots": []
     },
     {
       "id": "1-sts-philip--james-catholic-church-360-view-2",
-      "name": "Sts. Philip & James Catholic Church 360 View 2",
+      "name": "360 View 2",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
+      "faceSize": 1500,
+      "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        {
-          "yaw": 0.00006944264885255791,
-          "pitch": 0.019982600256007288,
-          "rotation": 0,
-          "target": "2-sts-philip--james-catholic-church-360-view-3"
-        },
-        {
-          "yaw": -0.031741485559042104,
-          "pitch": -1.2413255012899782,
-          "rotation": 0,
-          "target": "0-sts-philip--james-catholic-church-360-view-1"
-        }
+        { "yaw": 0.00006, "pitch": 0.019, "rotation": 0, "target": "2-sts-philip--james-catholic-church-360-view-3" },
+        { "yaw": -0.031, "pitch": -1.241, "rotation": 0, "target": "0-sts-philip--james-catholic-church-360-view-1" }
       ],
       "infoHotspots": []
     },
     {
       "id": "2-sts-philip--james-catholic-church-360-view-3",
-      "name": "Sts. Philip & James Catholic Church 360 View 3",
+      "name": "360 View 3",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
+      "faceSize": 1500,
+      "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        {
-          "yaw": -3.12932013875691,
-          "pitch": 0.22507832901270497,
-          "rotation": 0,
-          "target": "1-sts-philip--james-catholic-church-360-view-2"
-        },
-        {
-          "yaw": -3.136072085795199,
-          "pitch": -0.7684165747706437,
-          "rotation": 0,
-          "target": "0-sts-philip--james-catholic-church-360-view-1"
-        },
-        {
-          "yaw": -0.01404331499889544,
-          "pitch": 0.23073981679604216,
-          "rotation": 0,
-          "target": "3-sts-philip--james-catholic-church-360-view-4"
-        }
+        { "yaw": -3.129, "pitch": 0.225, "rotation": 0, "target": "1-sts-philip--james-catholic-church-360-view-2" },
+        { "yaw": -3.136, "pitch": -0.768, "rotation": 0, "target": "0-sts-philip--james-catholic-church-360-view-1" },
+        { "yaw": -0.014, "pitch": 0.230, "rotation": 0, "target": "3-sts-philip--james-catholic-church-360-view-4" }
       ],
       "infoHotspots": []
     },
     {
       "id": "3-sts-philip--james-catholic-church-360-view-4",
-      "name": "Sts. Philip & James Catholic Church 360 View 4",
+      "name": "360 View 4",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
+      "faceSize": 1500,
+      "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        {
-          "yaw": -0.013232536863295508,
-          "pitch": 0.19331513589873595,
-          "rotation": 0,
-          "target": "4-sts-philip--james-catholic-church-360-view-5"
-        },
-        {
-          "yaw": 3.1330579136074306,
-          "pitch": 0.21151266670598368,
-          "rotation": 0,
-          "target": "2-sts-philip--james-catholic-church-360-view-3"
-        }
+        { "yaw": -0.013, "pitch": 0.193, "rotation": 0, "target": "4-sts-philip--james-catholic-church-360-view-5" },
+        { "yaw": 3.133, "pitch": 0.211, "rotation": 0, "target": "2-sts-philip--james-catholic-church-360-view-3" }
       ],
       "infoHotspots": []
     },
     {
       "id": "4-sts-philip--james-catholic-church-360-view-5",
-      "name": "Sts. Philip & James Catholic Church 360 View 5",
+      "name": "360 View 5",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
+      "faceSize": 1500,
+      "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        {
-          "yaw": -3.1276478829659293,
-          "pitch": 0.2533720379371953,
-          "rotation": 0,
-          "target": "3-sts-philip--james-catholic-church-360-view-4"
-        },
-        {
-          "yaw": 0.0074376159140481235,
-          "pitch": 0.17957059116064222,
-          "rotation": 0,
-          "target": "5-sts-philip--james-catholic-church-360-view-6"
-        }
+        { "yaw": -3.127, "pitch": 0.253, "rotation": 0, "target": "3-sts-philip--james-catholic-church-360-view-4" },
+        { "yaw": 0.007, "pitch": 0.179, "rotation": 0, "target": "5-sts-philip--james-catholic-church-360-view-6" }
       ],
       "infoHotspots": []
     },
     {
       "id": "5-sts-philip--james-catholic-church-360-view-6",
-      "name": "Sts. Philip & James Catholic Church 360 View 6",
+      "name": "360 View 6",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
+      "faceSize": 1500,
+      "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        {
-          "yaw": 0.002423624652093892,
-          "pitch": 0.17740994907990348,
-          "rotation": 0,
-          "target": "6-sts-philip--james-catholic-church-360-view-7"
-        },
-        {
-          "yaw": 3.1198820210084426,
-          "pitch": 0.3581423216385229,
-          "rotation": 0,
-          "target": "4-sts-philip--james-catholic-church-360-view-5"
-        },
-        {
-          "yaw": 3.1198227646928194,
-          "pitch": 0.127747488261722,
-          "rotation": 0,
-          "target": "4-sts-philip--james-catholic-church-360-view-5"
-        }
+        { "yaw": 0.002, "pitch": 0.177, "rotation": 0, "target": "6-sts-philip--james-catholic-church-360-view-7" },
+        { "yaw": 3.119, "pitch": 0.358, "rotation": 0, "target": "4-sts-philip--james-catholic-church-360-view-5" }
       ],
       "infoHotspots": []
     },
     {
       "id": "6-sts-philip--james-catholic-church-360-view-7",
-      "name": "Sts. Philip & James Catholic Church 360 View 7",
+      "name": "360 View 7",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 },
+        { "tileSize": 512, "size": 1024 }
       ],
-      "faceSize": 3000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
+      "faceSize": 1500,
+      "initialViewParameters": { "pitch": 0, "yaw": 0, "fov": 1.4 },
       "linkHotspots": [
-        {
-          "yaw": -0.0016457052747469447,
-          "pitch": 0.5957203906574637,
-          "rotation": 0,
-          "target": "5-sts-philip--james-catholic-church-360-view-6"
-        },
-        {
-          "yaw": -0.00014487373090688038,
-          "pitch": 0.2283142248478338,
-          "rotation": 0,
-          "target": "4-sts-philip--james-catholic-church-360-view-5"
-        }
+        { "yaw": -0.001, "pitch": 0.595, "rotation": 0, "target": "5-sts-philip--james-catholic-church-360-view-6" },
+        { "yaw": -0.0001, "pitch": 0.228, "rotation": 0, "target": "4-sts-philip--james-catholic-church-360-view-5" }
       ],
       "infoHotspots": []
     }
@@ -352,7 +119,7 @@ var APP_DATA = {
   "name": "Sts. Philip & James Catholic Church Meyersdale PA",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": true,
     "viewControlButtons": false
   }
